0dacd5937264e425deee99e31573e5b7e3ece7b5
mineclover <mineclover@naver.com>
2022-07-03 10:28:20 +0900

    챕터 2,3 이론 설명 부분 구현

---
+++ src/chapter_02/MyButton.js
@@ -0,0 +1,14 @@
+//불러오는 index에 있다
+
+function MyButton(props){
+  const [isClicked, setIsClicked] = React.useState(false);
+
+  return React.createElement(
+    'button',
+    {onClick: () => setIsClicked(true) },
+    isClicked ? 'Clicked!' : 'Click here!'
+    )
+  
+}
+const domContainer = document.querySelector('#root');
+ReactDOM.render(React.createElement(MyButton),domContainer);
